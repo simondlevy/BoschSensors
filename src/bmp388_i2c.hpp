f096bbb46a8946751f07be234f6fca9b6db2d3c5
simondlevy <simon.d.levy@gmail.com>
2023-10-29 20:17:29 -0400

    Restored primary I2C address

--- src/bmp388_i2c.hpp
+++ src/bmp388_i2c.hpp
@@ -9,7 +9,7 @@ class BMP388_I2C : public BMP388 {
 
         virtual void devInit(BMP388::dev_t & dev) override
         {
-            dev.dev_id = BMP3_I2C_ADDR_SEC;
+            dev.dev_id = BMP3_I2C_ADDR_PRIM;
             dev.intf = BMP3_I2C_INTF;
             dev.read = (bmp3_com_fptr_t)i2c_burst_read;
             dev.write = (bmp3_com_fptr_t)i2c_burst_write;
