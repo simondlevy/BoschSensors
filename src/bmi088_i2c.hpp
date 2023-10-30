37e2e8bb1e136690bb62b529127dc7605cdea05e
simondlevy <simon.d.levy@gmail.com>
2023-10-29 21:54:49 -0400

    =More BMI088_I2C class work

--- src/bmi088_i2c.hpp
+++ src/bmi088_i2c.hpp
@@ -1,6 +1,28 @@
 #pragma once
 
+#include "vendor/bstdr_types.h"
+#include "vendor/bmi088_defs.h"
+
+bstdr_ret_t i2c_burst_read(uint8_t dev_id, uint8_t reg_addr,
+                              uint8_t *reg_data, uint16_t len);
+
+bstdr_ret_t i2c_burst_write(uint8_t dev_id, uint8_t reg_addr,
+                               uint8_t *reg_data, uint16_t len);
 
 class BMI088_I2C {
+
+    public:
+
+        BMI088_I2C(void)
+        {
+            _dev.gyro_id = BMI088_GYRO_I2C_ADDR_SECONDARY;
+            _dev.interface = BMI088_I2C_INTF;
+            _dev.read = (bmi088_com_fptr_t)i2c_burst_read;
+            _dev.write = (bmi088_com_fptr_t)i2c_burst_write;
+        }
+
+    private:
+
+        bmi088_dev_t _dev;
 
 };
