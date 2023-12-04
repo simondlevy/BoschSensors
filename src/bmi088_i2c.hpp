34c3cff177a95c607af97e6cbbd0700e8eb51a34
simondlevy <simon.d.levy@gmail.com>
2023-12-04 17:13:48 -0500

    Use static const int8_t instead of enum for error types

--- src/bmi088_i2c.hpp
+++ src/bmi088_i2c.hpp
@@ -27,50 +27,35 @@ class BMI088_I2C {
 
             _dev.delay_ms = delay;
 
-            return gyro_init(gyroFs) && accelInit();
+            struct bmi088_int_cfg intConfig;
+
+            _dev.gyro_cfg.power = BMI088_GYRO_PM_NORMAL;
+
+            auto rslt = BSTDR_OK;
+
+            rslt |= bmi088_set_gyro_power_mode(&_dev);
+
+            _dev.gyro_cfg.bw = BMI088_GYRO_BW_116_ODR_1000_HZ;
+            _dev.gyro_cfg.range = gyroFs;
+            _dev.gyro_cfg.odr = BMI088_GYRO_BW_116_ODR_1000_HZ;
+            rslt |= bmi088_set_gyro_meas_conf(&_dev);
+
+            intConfig.gyro_int_channel = BMI088_INT_CHANNEL_3;
+            intConfig.gyro_int_type = BMI088_GYRO_DATA_RDY_INT;
+            intConfig.gyro_int_pin_3_cfg.enable_int_pin = 1;
+            intConfig.gyro_int_pin_3_cfg.lvl = 1;
+            intConfig.gyro_int_pin_3_cfg.output_mode = 0;
+
+            rslt |= bmi088_set_gyro_int_config(&intConfig, &_dev);
+
+            delay(50);
+            struct bmi088_sensor_data gyr;
+            rslt |= bmi088_get_gyro_data(&gyr, &_dev);
+
+            return rslt == BSTDR_OK;
         }
 
     private:
 
         bmi088_dev_t _dev;
-
-        bool gyro_init(const uint8_t gyroFs)
-        {
-            auto rslt = bmi088_gyro_init(&_dev);
-
-            if (rslt == BSTDR_OK) {
-
-                struct bmi088_int_cfg intConfig;
-
-                _dev.gyro_cfg.power = BMI088_GYRO_PM_NORMAL;
-                rslt |= bmi088_set_gyro_power_mode(&_dev);
-
-                _dev.gyro_cfg.bw = BMI088_GYRO_BW_116_ODR_1000_HZ;
-                _dev.gyro_cfg.range = gyroFs;
-                _dev.gyro_cfg.odr = BMI088_GYRO_BW_116_ODR_1000_HZ;
-                rslt |= bmi088_set_gyro_meas_conf(&_dev);
-
-                intConfig.gyro_int_channel = BMI088_INT_CHANNEL_3;
-                intConfig.gyro_int_type = BMI088_GYRO_DATA_RDY_INT;
-                intConfig.gyro_int_pin_3_cfg.enable_int_pin = 1;
-                intConfig.gyro_int_pin_3_cfg.lvl = 1;
-                intConfig.gyro_int_pin_3_cfg.output_mode = 0;
-
-                rslt = bmi088_set_gyro_int_config(&intConfig, &_dev);
-
-                delay(50);
-                struct bmi088_sensor_data gyr;
-                rslt |= bmi088_get_gyro_data(&gyr, &_dev);
-            }
-
-            return rslt == BSTDR_OK;
-        }
-
-        bool accelInit(void)
-        {
-            auto rslt = bmi088_accel_switch_control(
-                    &_dev, BMI088_ACCEL_POWER_ENABLE);
-
-            return rslt == BSTDR_OK;
-        }
 };
